bc47631dee6b29a93e0e33cc9cdf8be978c2c64b
Leilani <pquaby@gmail.com>
2024-07-02 18:01:18 -0400

    Add dashboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   
   def show
+    @category_count = Category.count
+    @product_count = Product.count
   end
 end

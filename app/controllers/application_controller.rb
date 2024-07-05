4b775bbe60522eb8944039283ddfd7a11c3f1433
Leilani <pquaby@gmail.com>
2024-07-05 13:35:07 -0400

    Fix sign in not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   private
 
@@ -21,7 +22,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
@@ -30,14 +30,9 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
-    # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 
   def authorize
     redirect_to '/login' unless current_user
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative "application"
+require 'logger'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+log = Logger.new(STDOUT)
+error_log = Logger.new(STDERR)
+error_log = error_log.error("fatal error")

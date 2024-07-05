7af8cbdec2fc26e16c32414745f35e1c8db028f4
Leilani <pquaby@gmail.com>
2024-07-05 12:48:59 -0400

    Fix display issues with sign up and login pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]

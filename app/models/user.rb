8aefa16ea328eb24145a47a268c0f9a94f3351ca
Leilani <pquaby@gmail.com>
2024-07-05 12:12:04 -0400

    Add user model and db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
 
   has_secure_password
-
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
 end

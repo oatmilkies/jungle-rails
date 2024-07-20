4c39dc8a710f2f3d73e804c29583c59768e3dbf7
Leilani <pquaby@gmail.com>
2024-07-20 10:54:30 -0400

    Add home page tests

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,13 @@
+describe('Home Page', () => {
+  it('should visit the home page', () => {
+    cy.visit('/');
+  });
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it('There are 2 products on the page', () => {
+    cy.get('.products article').should('have.length', 2);
+  });
+});
